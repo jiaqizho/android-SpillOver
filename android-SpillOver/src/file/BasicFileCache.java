58de14a7e3548eb34b24e61125615f421e667b14
smali <smalinuxer@gmail.com>
2014-08-20 12:34:01 +0800

    branch test

--- android-SpillOver/src/file/BasicFileCache.java
+++ android-SpillOver/src/file/BasicFileCache.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStream; 
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
