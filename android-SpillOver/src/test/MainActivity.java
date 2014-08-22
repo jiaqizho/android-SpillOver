6e3dcdbae516a9343b22a78f38ccbdad8ae3cb8d
smali <smalinuxer@gmail.com>
2014-08-22 18:18:30 +0800

    删了没用的

--- android-SpillOver/src/test/MainActivity.java
+++ android-SpillOver/src/test/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainview);
-
+	
 		
 		new Thread(new Runnable() { 
 			
